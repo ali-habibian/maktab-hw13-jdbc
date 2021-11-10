0e83798aceb225fecc3f6b0d20c597b9987e5d3d
Ali Habibian <ali.habibian04@gmail.com>
2021-11-10 21:49:44 +0330

    Completed AbstractCrudService

--- src/main/java/ir/maktab/jdbc/service/AbstractCrudService.java
+++ src/main/java/ir/maktab/jdbc/service/AbstractCrudService.java
@@ -3,8 +3,18 @@ package ir.maktab.jdbc.service;
 import ir.maktab.jdbc.dao.core.BaseDao;
 import ir.maktab.jdbc.entity.base.BaseEntity;
 
+import java.util.List;
+
 public class AbstractCrudService<T extends BaseEntity<ID>, ID extends Number> {
     private BaseDao<T, ID> baseDao;
+
+    public void setBaseDao(BaseDao<T, ID> baseDao) {
+        this.baseDao = baseDao;
+    }
+
+    public BaseDao<T, ID> getBaseDao() {
+        return baseDao;
+    }
 
     public void saveOrUpdate(T entity) {
         if (entity.getId() == null) {
@@ -14,11 +24,15 @@ public class AbstractCrudService<T extends BaseEntity<ID>, ID extends Number> {
         }
     }
 
-    public void setBaseDao(BaseDao<T, ID> baseDao) {
-        this.baseDao = baseDao;
+    public void deleteById(ID id){
+        baseDao.delete(id);
     }
 
-    public BaseDao<T, ID> getBaseDao() {
-        return baseDao;
+    public T loadById(ID id){
+        return baseDao.loadById(id);
+    }
+
+    public List<T> loadAll(){
+        return baseDao.loadAll();
     }
 }
